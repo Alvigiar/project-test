813de4e7bf6c66cfc07f9fab4f6c81936258b68f
Alvigiar <officialgmvladmedvedkov@gmail.com>
2021-06-20 20:40:10 +0300

    create burg

--- main.js
+++ main.js
@@ -4,13 +4,7 @@ import './node_modules/normalizecss/normalize.css'
 
 
 
-// document.querySelector('.btn').addEventListener('click', () => {
-//     document.querySelector('.menu__list').classList.toggle('active')
-// })
-
-const one = document.querySelector('btn')
-const two = document.querySelector('.menu__list')
-
-one.addEventListener('click', function () {
-    two.classList.toggle('active')
+document.querySelector('.burger__cell').addEventListener('click', () => {
+    document.querySelector('.menu__list').classList.toggle('active'),
+    document.querySelector('.burger__menu').classList.toggle('cross')
 })
