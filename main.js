7afca58a201bb163d9671cc90ba9535ed67be6ae
Alvigiar <vladislav-medvedkov@gmail.com>
2021-06-30 12:41:06 +0300

    question swith..case

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 import './main.scss'
-import jquery from 'jquery';
+// import jquery from 'jquery'; // ноут не импортит
 
-window.$ = jquery;
+// window.$ = jquery;
 
 
 // document.querySelector('.burger__cell').addEventListener('click', () => {
@@ -70,14 +70,84 @@ window.$ = jquery;
 
 // 5
 
-let one = prompt('Имя 1', 1)
-let two = prompt('Имя 2', 2)
-let fri = prompt('Имя 3', 3)
-let sum = [`${one} ${two} ${fri}`]
+// let one = prompt('Имя 1', 'Благоразумие')
+// let two = prompt('Имя 2', 'Справедливость')
+// let three = prompt('Имя 3', 'Умеренность')
+// let four = prompt('Имя 4', 'Мужество')
+// let sum = [`${one} ${two} ${three} ${four}`]
 // console.log(sum)
 
-sum.forEach(element => {
-  console.log(element)
-  let div = document.createElement('div')
-  $(div).append(`${element}`)
-});
+// sum.forEach(element => {
+//   let div = document.createElement('div')
+//   div.className = 'test'
+//   div.innerHTML = `${element}`
+//   document.body.append(div)
+//   console.log(element)
+// });
+
+// Условные конструкции if...else, switch ... case, тернарный оператор
+
+// 1
+
+// let name = prompt('Name')
+// let birth = prompt('Year Birth')
+// let now = new Date();
+// let year = now.getFullYear()
+// let age = +year - +birth
+// console.log(age)
+// document.body.innerHTML = name + ': ' + age
+
+// 2
+
+let floor = +prompt('Floor')
+// let floor = 10
+let stepFloor2_4 = 20
+let stepFloor5_7 = 30
+let stepFloor8_10 = 40
+
+let floor2 = +stepFloor2_4
+let floor3 = +stepFloor2_4 * 2
+let floor4 = +stepFloor2_4 * 3
+
+let floor5 = +floor4 + +stepFloor5_7
+let floor6 = +floor4 + +stepFloor5_7 * 2
+let floor7 = +floor4 + +stepFloor5_7 * 3
+
+let floor8 = +floor7 + +stepFloor8_10
+let floor9 = +floor7 + +stepFloor8_10 * 2
+let floor10 = +floor7 + +stepFloor8_10 * 3
+
+switch (floor) {
+  case 2:
+    console.log(floor2)
+    break;
+  case 3:
+    console.log(floor3)
+    break;
+  case 4:
+    console.log(floor4)
+    break;
+  case 5:
+    console.log(floor5)
+    break;
+  case 6:
+    console.log(floor6)
+    break;
+  case 7:
+    console.log(floor7)
+    break;
+  case 8:
+    console.log(floor8)
+    break;
+  case 9:
+    console.log(floor9)
+    break;
+  case 10:
+    console.log(floor10)
+    break;
+
+    
+  default:
+    console.log('-')
+    break;
+}
